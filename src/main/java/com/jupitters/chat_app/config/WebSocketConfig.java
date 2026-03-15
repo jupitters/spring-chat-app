44ea029e283331daab66d31fe50aac15085ed5b1
jupitters <sampaiobale@gmail.com>
2026-03-15 14:56:51 -0300

    we socket config

--- src/main/java/com/jupitters/chat_app/config/WebSocketConfig.java
+++ src/main/java/com/jupitters/chat_app/config/WebSocketConfig.java
@@ -18,6 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 }
