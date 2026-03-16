a64a4bc01b358015014f483d304c50ae0fcdbb06
jupitters <sampaiobale@gmail.com>
2026-03-16 12:58:08 -0300

    controller

--- src/main/java/com/jupitters/chat_app/controller/ChatController.java
+++ src/main/java/com/jupitters/chat_app/controller/ChatController.java
@@ -1,9 +1,14 @@
 package com.jupitters.chat_app.controller;
 
+import com.jupitters.chat_app.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
+    @MessageMapping("/send-message")
+    @SendTo("/topic/messages")
     public ChatMessage sendMessage(ChatMessage message) {
         return message;
     }
