45d709e2bc7a1b6a783e7a6437395086dba1c275
jupitters <sampaiobale@gmail.com>
2026-03-16 13:41:23 -0300

    controller

--- src/main/java/com/jupitters/chat_app/controller/ChatController.java
+++ src/main/java/com/jupitters/chat_app/controller/ChatController.java
@@ -4,6 +4,7 @@ import com.jupitters.chat_app.model.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
@@ -13,6 +14,7 @@ public class ChatController {
         return message;
     }
 
+    @GetMapping("/chat")
     public String chat(){
         return "chat";
     }
