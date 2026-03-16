d18814c3754840ab48e056a492c57fb570025742
jupitters <sampaiobale@gmail.com>
2026-03-16 12:50:48 -0300

    controller

--- src/main/java/com/jupitters/chat_app/controller/ChatController.java
+++ src/main/java/com/jupitters/chat_app/controller/ChatController.java
@@ -1,4 +1,10 @@
 package com.jupitters.chat_app.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ChatController {
+    public ChatMessage sendMessage(ChatMessage message) {
+        return message;
+    }
 }
