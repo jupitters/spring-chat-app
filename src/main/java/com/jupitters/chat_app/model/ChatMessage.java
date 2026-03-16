7da2aff143ec08e33b168ab5f167e8a034d7692f
jupitters <sampaiobale@gmail.com>
2026-03-16 12:52:42 -0300

    chat message model

--- src/main/java/com/jupitters/chat_app/model/ChatMessage.java
+++ src/main/java/com/jupitters/chat_app/model/ChatMessage.java
@@ -1,5 +1,12 @@
 package com.jupitters.chat_app.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatMessage {
     private Long id;
     private String sender;
